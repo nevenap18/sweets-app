7cef7278bcffa6aabbdc92b9d5a36ea2ad1755d3
Nevena Pavlovic <nevena.pavlovic@ugbb.net>
2020-05-11 18:29:10 +0200

    changed dev:start script and added a console messg

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+  console.log('\n' + 'finished, listening at ' + '\x1B[4m\u001b[32m' +  'http://localhost:' + port + '\u001b[0m\x1B[0m' + '\n')
 }
 bootstrap();
