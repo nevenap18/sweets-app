2ff99dd39c08e22b4896ab25843559a7a45539f3
Nevena Pavlovic <nevena.pavlovic@ugbb.net>
2020-05-13 21:42:45 +0200

    login and auth middleware

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,57 @@
+import { Controller, Post, Body, Req } from "@nestjs/common";
+import { AdministratorService } from "src/services/administrator/administrator.service";
+import { LoginAdministratorDto } from "src/dtos/administrator/login.administrator";
+import { ApiResponse } from "src/misc/api.response.class";
+import * as crypto from 'crypto';
+import { LoginInfoAdministratorDto } from "src/dtos/administrator/login.info.administrator.dto";
+import * as jwt from 'jsonwebtoken'
+import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator";
+import { Request } from "express";
+import { jwtSecret } from "config/jwt.secret";
+
+@Controller('auth') // http://localhost:3000/auth/
+export class AuthController {
+    constructor(
+        public administratorService: AdministratorService
+    ) { }
+
+    @Post('login')
+    async doLogin(@Body() data: LoginAdministratorDto, @Req() req: Request): Promise<LoginInfoAdministratorDto | ApiResponse> {
+      const admin = await this.administratorService.getByUsername(data.username)
+    
+      
+      if (!admin) {
+        return new Promise(resolve => resolve(new ApiResponse('error', -3001, 'wrong account username')))
+      }
+
+      const passwordHash = crypto.createHash('sha512')
+      passwordHash.update(data.password)
+      const passwordHashString = passwordHash.digest('hex').toUpperCase()
+
+      if (admin.passwordHash !== passwordHashString) {
+        return new Promise(resolve => resolve(new ApiResponse('error', -3002, 'wrong account password')))
+      }
+
+      const jwtData = new JwtDataAdministratorDto()
+      jwtData.administratorId = admin.administratorId
+      jwtData.username = admin.username
+      let now = new Date()
+      now.setDate(now.getDate() + 14)
+      const expTime = now.getTime() / 1000
+      jwtData.exp = expTime
+      jwtData.ip = req.ip.toString()
+      jwtData.ua = req.headers["user-agent"]
+
+      let token: string = jwt.sign(jwtData.toPlainObject(), jwtSecret)
+
+      const response = new LoginInfoAdministratorDto(
+        admin.administratorId,
+        admin.username,
+        token
+      )
+
+      return new Promise(resolve => resolve(response))
+
+    }
+
+  }
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,65 @@
+import { NestMiddleware, HttpStatus, HttpException, Injectable } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { AdministratorService } from "src/services/administrator/administrator.service";
+import * as jwt from "jsonwebtoken"
+import { jwtSecret } from "config/jwt.secret";
+import { JwtDataAdministratorDto } from "src/dtos/administrator/jwt.data.administrator";
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+
+  constructor (private readonly administratorService: AdministratorService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    
+    if (!req.headers.authorization) {
+      throw new HttpException('Token not found', HttpStatus.UNAUTHORIZED)
+    }
+    
+    const tokenParts = req.headers.authorization.split(' ')
+
+    if (tokenParts.length !== 2) {
+      throw new HttpException('Token does not exist!', HttpStatus.UNAUTHORIZED)
+    }
+
+    const token = tokenParts[1]
+    let jwtData: JwtDataAdministratorDto
+
+    try {
+      jwtData = jwt.verify(token, jwtSecret)
+
+      if (!jwtData) {
+        throw new HttpException('Token is not valid!', HttpStatus.UNAUTHORIZED);
+      }
+
+    } catch (e) {
+      throw new HttpException('Token is not valid!', HttpStatus.UNAUTHORIZED);
+    }
+
+    if (!jwtData) {
+      throw new HttpException('Bad token found', HttpStatus.UNAUTHORIZED)
+    }
+
+    if (jwtData.ip !== req.ip) {
+      throw new HttpException('Bad ip address', HttpStatus.UNAUTHORIZED)
+    }
+
+    if (jwtData.ua !== req.headers['user-agent']) {
+      throw new HttpException('Bad user agent', HttpStatus.UNAUTHORIZED)
+    }
+
+    const administrator = await this.administratorService.getById(jwtData.administratorId)
+
+    if (!administrator){
+      throw new HttpException('Account not found', HttpStatus.UNAUTHORIZED)
+    }
+    
+    const currentTime = new Date().getTime() / 1000
+
+    if (currentTime >= jwtData.exp) {
+      throw new HttpException('Token has expired', HttpStatus.UNAUTHORIZED)
+    }
+
+    next()
+  }
+
+}
